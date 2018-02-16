25598d7008bde89f3eba369231b1a5d6e267d600
Sean Wesenberg <wookets@gmail.com>
2018-02-15 21:54:58 -0600

    Minor support for some buttons (still not a calc)

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var output: UILabel!
+    
+    @IBOutlet weak var button0: UIButton!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    
+    
+    @IBAction func button0pushed(_ sender: Any) {
+        self.output.text = "0"
+    }
+    @IBAction func button1pushed(_ sender: Any) {
+        self.output.text = "1"
+    }
+    @IBAction func button2pushed(_ sender: Any) {
+        self.output.text = "2"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
